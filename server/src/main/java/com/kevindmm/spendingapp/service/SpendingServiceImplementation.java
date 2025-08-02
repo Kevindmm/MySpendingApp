fec652032b03daed32ca21e325daa673405ebccd
Kevin Montaño <kevindmm@outlook.es>
2025-08-02 18:51:47 +0100

    Replace String Keys with LocalDate and group total spending by day, month and year

--- server/src/main/java/com/kevindmm/spendingapp/service/SpendingServiceImplementation.java
+++ server/src/main/java/com/kevindmm/spendingapp/service/SpendingServiceImplementation.java
@@ -4,7 +4,6 @@ import com.kevindmm.spendingapp.model.Transaction;
 import com.kevindmm.spendingapp.repository.TransactionRepository;
 import com.kevindmm.spendingapp.util.DateRange;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.*;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +12,9 @@ public class SpendingServiceImplementation implements SpendingService {
 
   private final TransactionRepository transactionRepository;
   private final DateRange rangeResolver;
+
+  public static final String DAILY = "daily";
+  public static final String MONTHLY = "monthly";
 
   public SpendingServiceImplementation(
     TransactionRepository transactionRepository,
@@ -30,39 +32,39 @@ public class SpendingServiceImplementation implements SpendingService {
       dateRange[1]
     );
 
-    Map<String, Float> totalAmountMap = new HashMap<>();
+    Map<LocalDate, Float> totalAmountMap = new HashMap<>();
 
     transactions.forEach(transaction -> {
-      LocalDate date = LocalDate.of(
-        transaction.getDate().getYear(),
-        transaction.getDate().getMonth(),
-        transaction.getDate().getDayOfMonth()
-      );
-      String dateString = date.toString();
+      LocalDate dateKey;
 
-      if (totalAmountMap.containsKey(dateString)) {
-        totalAmountMap.replace(
-          dateString,
-          totalAmountMap.get(dateString) + transaction.getAmount()
-        );
+      if (DAILY.equalsIgnoreCase(frame)) {
+        dateKey = transaction.getDate();
+      } else if (MONTHLY.equalsIgnoreCase(frame)) {
+        dateKey = transaction.getDate().withDayOfMonth(1);
       } else {
-        totalAmountMap.put(dateString, transaction.getAmount());
+        dateKey = transaction.getDate().withDayOfYear(1);
+      }
+
+      if (totalAmountMap.containsKey(dateKey)) {
+        totalAmountMap.put(dateKey, totalAmountMap.get(dateKey) + transaction.getAmount());
+      } else {
+        totalAmountMap.put(dateKey, transaction.getAmount());
       }
     });
 
     List<Map<String, Object>> formattedSpendingData = new ArrayList<>();
 
-    for (String date : totalAmountMap.keySet()) {
+    for (LocalDate date : totalAmountMap.keySet()) {
       Map<String, Object> tmpMap = new HashMap<>();
       tmpMap.put("totalAmount", totalAmountMap.get(date));
-      tmpMap.put("startDate", LocalDate.parse(date).atStartOfDay().toString());
+      tmpMap.put("startDate", date.atStartOfDay().toString());
       formattedSpendingData.add(tmpMap);
     }
 
-    formattedSpendingData.sort(
+/*    formattedSpendingData.sort(
       Comparator.comparing(o -> LocalDateTime.parse((String) o.get("startDate"))
       )
-    );
+    );*/
 
     return formattedSpendingData;
   }
